84cffb798fd444c7463ec5213aa72fd9d138adad
jkh8360 <jkh8360@gmail.com>
2025-09-05 13:01:35 +0900

    홈 에러 화면 scopeKey 추가

--- src/domain/device/hooks/queries/useGetDeviceStatus.ts
+++ src/domain/device/hooks/queries/useGetDeviceStatus.ts
@@ -4,16 +4,27 @@ import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "../../constants/queryKey";
 import { getDeviceConn } from "../../api";
 
+type DeviceConnParams = RequestDeviceConn & {
+    scopeKey?: string[];
+};
+
 function useGetDeviceStatus(
-    params: RequestDeviceConn,
+    params: DeviceConnParams,
     queryOptions?: UseQueryCustomOptions<ResponseDeviceConn>,
     ) {
+    const { scopeKey = [], ...apiParams } = params;
+
     return useQuery({
-        queryKey: [queryKeys.DEVICE, queryKeys.GET_DEVICE_CONN, params],
-        queryFn: () => getDeviceConn(params),
+        queryKey: [
+            ...scopeKey,
+            queryKeys.DEVICE, 
+            queryKeys.GET_DEVICE_CONN, 
+            apiParams
+        ],
         meta: {
-        showGlobalSpinner: false,
+            showGlobalSpinner: scopeKey.length === 0, // scopeKey가 없을 때만 글로벌 스피너 표시
         },
+        queryFn: () => getDeviceConn(apiParams),
 
         // 1. 데이터를 영원히 fresh 상태로 설정하여 자동 재요청 방지
         staleTime: 2 * 1000, // 2초 동안 fresh 상태 유지
